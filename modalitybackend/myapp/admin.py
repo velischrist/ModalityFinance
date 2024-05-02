14e4c8e21700e99289eb8121419721088ab6b335
velischrist <velis.christodoulou@gmail.com>
2024-05-02 15:28:53 -0700

    Tested database entry via Django

--- modalitybackend/myapp/admin.py
+++ modalitybackend/myapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Company 
+from .models import Company, Lp
 
 admin.site.register(Company)
+admin.site.register(Lp)
