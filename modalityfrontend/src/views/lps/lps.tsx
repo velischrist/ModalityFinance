e19aec34ee5272e2aa39b8fcd405cfe3785a0d7e
strbotis <strbotis+1@gmail.com>
2024-05-02 18:18:31 -0700

    Dialog

---
+++ modalityfrontend/src/views/lps/lps.tsx
@@ -0,0 +1,51 @@
+// // /** Example purpose only */
+// // const lps = () => {
+// //     return <div>My LPs</div>
+// // }
+
+// // export default lps
+
+// // import InternalScroll from './LPformdialog'
+// // import Filtering from './LPlist'
+
+// // export { InternalScroll, Filtering }
+// import Tabs from '@/components/ui/Tabs'
+
+// const { TabNav, TabList, TabContent } = Tabs
+
+// const Default = () => {
+//     return (
+//         <div>
+//             <h3 className="mb-5 lg:mb-5">Organization Settings</h3>
+//             <Tabs defaultValue="tab1">
+//                 <TabList>
+//                     <TabNav value="tab1">LPs Details</TabNav>
+//                     <TabNav value="tab2">Commitments</TabNav>
+//                     <TabNav value="tab3">Organization</TabNav>
+//                 </TabList>
+//                 <div className="p-4">
+//                     <TabContent value="tab1">
+//                         <FilteringFund></FilteringFund>
+//                     </TabContent>
+//                     <TabContent value="tab2">
+//                         <p>
+//                             A computer lets you make more mistakes faster than
+//                             any invention in human history with the possible
+//                             exceptions of handguns and tequila. (Mitch
+//                             Radcliffe).
+//                         </p>
+//                     </TabContent>
+//                     <TabContent value="tab3">
+//                         <p>
+//                             In C++ its harder to shoot yourself in the foot, but
+//                             when you do, you blow off your whole leg. (Bjarne
+//                             Stroustrup)
+//                         </p>
+//                     </TabContent>
+//                 </div>
+//             </Tabs>
+//         </div>
+//     )
+// }
+
+// export default Default
